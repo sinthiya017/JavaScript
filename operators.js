59c1fc049ef84ba6ba8674491dd71d1c66c51720
sinthiyav <sinthya017.v@gmail.com>
2024-01-21 20:34:14 -0800

    javascript

---
+++ operators.js
@@ -0,0 +1,25 @@
+//Assignment Operator//
+var a=10
+console.log("Assignment Operator is",a)
+
+//Arithmetic Operator//
+var aA=10
+var bA=10
+console.log(typeof aA) //to check the type
+console.log("Arithmetic Operator Addition Output:",aA+bA)
+console.log("Arithmetic Operator Subtraction Output:",aA-bA)
+console.log("Arithmetic Operator Multiplication Output:",aA*bA)
+console.log("Arithmetic Operator Division Output:",aA/bA)
+console.log("Arithmetic Operator modulus Output:",aA%bA)
+
+//Logical Operator ->,||,&&,!,=>//
+var aL=10
+var bL=20
+console.log("Logical Operator &&",aL&&bL)
+console.log("Logical Operator ||",aL||bL)
+console.log("Logical Operator !",!aL)
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,11 @@
+a='a'
+console.log("general variable declaration a=",a)
+var b='var'
+console.log("This variable belongs to var type value is var b='var':",b)
+b=10
+console.log("This variable belongs to var type value is :",b)
+var c='let'
+console.log("This variable belongs to var type value is var c='let':",c)
+const d=100
+d=200                                              
+console.log("constant Variable value is const d=100 :",d)
