d33def6adcc6d54e12597792095451f0c674f41a
sinthiyav <sinthya017.v@gmail.com>
2024-01-23 09:35:42 -0800

    js

--- callback.js
+++ callback.js
@@ -1,5 +1,5 @@
 //CALLBACK
-function somuRamu(welcomemessage,callback){
+/*function somuRamu(welcomemessage,callback){
     setTimeout(function(){
         console.log(welcomemessage)
         callback();
@@ -8,6 +8,40 @@ function somuRamu(welcomemessage,callback){
 
 }
 function aniMini(){
-    console.log("Not come to clge")
+    console.log("Not come to clg")
 }
 somuRamu("Hello Somu and Ramu\nWelcome to KEC",aniMini)
+
+
+//form submission example
+
+function submitForm(buttonclick,db){
+    setTimeout(function(){
+        console.log(buttonclick)
+        db()
+    },2000)
+
+}
+
+function database(){
+    console.log("form submitted successfully")
+}
+
+
+submitForm("Form submit button clicked",database)
+
+function password(correct,ps){
+    setTimeout(function(){
+        console.log(correct)
+        ps()
+
+    },2000)
+}
+
+
+function passcheck(){
+    console.log("Password is correct")
+}
+
+
+password("please enter your passwowrd",passcheck)
